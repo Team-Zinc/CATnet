d88581d0b0626b300b85d76d35d7ffe539550146
Isacc Barker <plutoisplanet4324@gmail.com>
2021-02-10 14:35:16 -0700

    Added google test/mock unit testing/mocking

---
+++ src/head/head.hpp
@@ -0,0 +1,6 @@
+#ifndef HEAD_HPP
+#define HEAD_HPP
+
+const int example_int = 10;
+
+#endif /* HEAD_HPP */
---
+++ src/whisker/whisker.hpp
@@ -0,0 +1,6 @@
+#ifndef WHISKER_HPP
+#define WHISKER_HPP
+
+const int example_int = 10;
+
+#endif /* WHISKER_HPP */
