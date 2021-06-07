ce23477373207a78da60829af4df4919b9e5d4e9
Isacc Barker <plutoisplanet4324@gmail.com>
2021-06-06 23:20:02 -0600

    Participant can send messages and establish connection to registrar

---
+++ src/participant/whisker_exchange.cpp
@@ -0,0 +1,15 @@
+#include "whisker_exchange.hpp"
+
+#include <iostream>
+
+void WhiskerExchange::init() {
+
+}
+
+void WhiskerExchange::run() {
+
+}
+
+void WhiskerExchange::destroy() {
+
+}
---
+++ src/participant/whisker_exchange.hpp
@@ -0,0 +1,11 @@
+#ifndef WHISKER_EXCHANGE_HPP
+#define WHISKER_EXCHANGE_HPP
+
+class WhiskerExchange {
+public:
+    static void init();
+    static void destroy();
+    static void run();
+};
+
+#endif
