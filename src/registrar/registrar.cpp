002764b10e1898df2a632237259a23b13a210a07
Eli Hatton <elihatton@zincsoft.dev>
2021-06-24 17:16:00 -0600

    putting this in github so I can force WSL to kernel panic! yay!

--- src/registrar/registrar.cpp
+++ src/registrar/registrar.cpp
@@ -1,14 +1,51 @@
 #include "registrar.hpp"
+#include <tcp_socket.hpp>
+#include <tcpserver_socket.hpp>
+#include <thread>
+#include <message.hpp>
 
 #include "../logging/include/log.hpp"
 
 Registrar::Registrar() {
     CND_REGISTRAR_TRACE("Constructing registrar....");
+
+    // NOTICE: threads will be implemented when it is neccicary for the registrar to do multiple things at once. 
+    // Currently, all the registrar needs to do is connect to a wasker and inform it that it has connected
+
+    // this section will also deal with storing participant info with JSON in the future
+
+
 }
+
+void Registrar::tcp_init()
+{
+    CND_REGISTRAR_TRACE("Creating tcp server socket for registrar...");
+    head.emplace("localhost", 44400);
+}
+
 
 void Registrar::run() {
     CND_REGISTRAR_TRACE("Running in registrar mode....");
+    if (! head.has_value())
+    {
+        tcp_init();
+    }
+
+    CND_REGISTRAR_TRACE("Listening for connections from participants...");
+
+    head->acceptConnection();
+
+    if (head->isConnected)
+    {
+        CND_REGISTRAR_TRACE("Connected to a participant! creating recieved message...");
+
+        Message recieved;
+
+    }
+
+    
 }
+
 
 Registrar::~Registrar() {
     CND_REGISTRAR_TRACE("Destructing registrar....");
--- src/registrar/registrar.hpp
+++ src/registrar/registrar.hpp
@@ -1,3 +1,9 @@
+#ifndef REGISTRAR_HPP
+#define REDISTRAR_HPP
+
+#include <vector>
+#include <optional>
+#include <tcp_server_socket.hpp>
 /// \brief Registrar class that contains all the data needed for runtime in case of registrar mode.
 class Registrar {
 public:
@@ -13,5 +19,14 @@ public:
     /// If you need to pass in any arguments, please add them to the
     /// subcommand, and pass that in here. Do not pass them in individually.
     void run();
+
+    // Opens a tcp server socket for the registrar to make connections on
+    void tcp_init();
+
+    private:
+        inline static std::optional<TcpServerSocket> head;
+
+    
+
 };
 
