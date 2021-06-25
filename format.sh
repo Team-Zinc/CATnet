2385ec32f268bb1f23f1bb5af3ba1ade2c546a03
Isacc Barker <plutoisplanet4324@gmail.com>
2021-06-25 00:11:32 -0600

    fixed bug where format files could become options

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
-for file in $(find src -iname *.hpp -o -iname *.cpp -o -iname *.c -iname *.h); do
+for file in $(find src -iname ./*.hpp -o -iname ./*.cpp -o -iname ./*.c -iname ./*.h); do
     echo -e "\033[A\r\033[K$file"
     clang-format -i --style=file $file
 done
