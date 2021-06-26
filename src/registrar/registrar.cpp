d194d052a7cefe07006370f9f69685038e996886
Clang Robot <milobanks@zincsoft.dev>
2021-06-26 20:53:28 +0000

    Committing clang-format changes

--- src/registrar/registrar.cpp
+++ src/registrar/registrar.cpp
@@ -1,55 +1,52 @@
 #include "registrar.hpp"
+#include <message.hpp>
 #include <tcp_socket.hpp>
 #include <tcpserver_socket.hpp>
 #include <thread>
-#include <message.hpp>
 
 #include "../logging/include/log.hpp"
 
-
-Registrar::Registrar() {
-    CND_REGISTRAR_TRACE("Constructing registrar....");
-
-    // NOTICE: threads will be implemented when it is neccicary for the registrar to do multiple things at once. 
-    // Currently, all the registrar needs to do is connect to a wasker and inform it that it has connected
-
-    // this section will also deal with storing participant info with JSON in the future
-
-
-}
-
-void Registrar::tcp_init()
+Registrar::Registrar()
 {
-    CND_REGISTRAR_TRACE("Creating tcp server socket for registrar...");
-    head.emplace("localhost", 44400);
+	CND_REGISTRAR_TRACE("Constructing registrar....");
+
+	// NOTICE: threads will be implemented when it is neccicary for the
+	// registrar to do multiple things at once. Currently, all the registrar
+	// needs to do is connect to a wasker and inform it that it has connected
+
+	// this section will also deal with storing participant info with JSON in
+	// the future
 }
 
-
-void Registrar::run() {
-    CND_REGISTRAR_TRACE("Running in registrar mode....");
-    if (! head.has_value())
-    {
-        tcp_init();
-    }
-
-    CND_REGISTRAR_TRACE("Listening for connections from participants...");
-    bool connected = false;
-    while (! connected)
-    {
-        head->acceptConnection();
-        if (head->isConnected)
-        {
-            CND_REGISTRAR_TRACE("Connected to a participant!");
-            connected = true;
-        }
-        else
-        {
-            CND_REGISTRAR_TRACE("No connection found, trying again...");
-        }
-    }
+void
+Registrar::tcp_init()
+{
+	CND_REGISTRAR_TRACE("Creating tcp server socket for registrar...");
+	head.emplace("localhost", 44400);
 }
 
+void
+Registrar::run()
+{
+	CND_REGISTRAR_TRACE("Running in registrar mode....");
+	if (!head.has_value()) {
+		tcp_init();
+	}
 
-Registrar::~Registrar() {
-    CND_REGISTRAR_TRACE("Destructing registrar....");
+	CND_REGISTRAR_TRACE("Listening for connections from participants...");
+	bool connected = false;
+	while (!connected) {
+		head->acceptConnection();
+		if (head->isConnected) {
+			CND_REGISTRAR_TRACE("Connected to a participant!");
+			connected = true;
+		} else {
+			CND_REGISTRAR_TRACE("No connection found, trying again...");
+		}
+	}
+}
+
+Registrar::~Registrar()
+{
+	CND_REGISTRAR_TRACE("Destructing registrar....");
 }
--- src/registrar/registrar.hpp
+++ src/registrar/registrar.hpp
@@ -1,31 +1,30 @@
 #ifndef REGISTRAR_HPP
 #define REDISTRAR_HPP
 
-#include <vector>
 #include <optional>
 #include <tcp_server_socket.hpp>
-/// \brief Registrar class that contains all the data needed for runtime in case of registrar mode.
-class Registrar {
-public:
-    /// \brief Constructor for the registrar.
-    /// If you need to pass in any arguments, please add them to the
-    /// subcommand, and pass that in here. Do not pass them in individually.
-    Registrar();
+#include <vector>
+/// \brief Registrar class that contains all the data needed for runtime in case
+/// of registrar mode.
+class Registrar
+{
+  public:
+	/// \brief Constructor for the registrar.
+	/// If you need to pass in any arguments, please add them to the
+	/// subcommand, and pass that in here. Do not pass them in individually.
+	Registrar();
 
-    /// \brief Destructor for the registrar.
-    ~Registrar();
-    
-    /// \brief Run the main code that actually gives the registrar purpose.
-    /// If you need to pass in any arguments, please add them to the
-    /// subcommand, and pass that in here. Do not pass them in individually.
-    void run();
+	/// \brief Destructor for the registrar.
+	~Registrar();
 
-    // Opens a tcp server socket for the registrar to make connections on
-    void tcp_init();
+	/// \brief Run the main code that actually gives the registrar purpose.
+	/// If you need to pass in any arguments, please add them to the
+	/// subcommand, and pass that in here. Do not pass them in individually.
+	void run();
 
-    private:
-        inline static std::optional<TcpServerSocket> head;
+	// Opens a tcp server socket for the registrar to make connections on
+	void tcp_init();
 
-    
-
+  private:
+	inline static std::optional<TcpServerSocket> head;
 };
